15df2c187108a46ba13c44984c6bbe29dcf2aabe
PhilipJames <pjkelnhofer@gmail.com>
2015-10-27 11:12:45 -0400

    Edited welcome to test DO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">p4 Welcome Page.</div>
             </div>
         </div>
     </body>
