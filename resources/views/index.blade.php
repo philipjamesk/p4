7d5c9f70a725f83b62512fb51fd4c9bbf39b097f
PhilipJames <pjkelnhofer@gmail.com>
2015-12-09 15:23:09 -0500

    Made footer sticky; fixed typo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <h1>Welcome to QuizMark</h1>
     <h3>The Online Quiz Host</h3>
     <hr>
-    <p>Quiz mark is an online quiz hosting platform. Read more about it <a href="/about">here</a>.</p>
+    <p>QuizMark is an online quiz hosting platform. Read more about it <a href="/about">here</a>.</p>
     @if(!Auth::check())
         <p>If you are student or a teacher who is already registered, you can <a href="/login">login here</a>.</p>
         <p>If you have not yet signed up, you can <a href="/register">register here</a> instead.</p>
