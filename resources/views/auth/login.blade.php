f30b0784b5fe7c9b2e5e6e868b30dd2899a4e4e1
PhilipJames <pjkelnhofer@gmail.com>
2015-12-09 12:25:33 -0500

    Fixed login and register POST actions

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                 <div class="panel-body">
                                  
                    @include('errors.errors')
-                    <form class="form-horizontal" role="form" method="POST" action="/auth/login">
+                    <form class="form-horizontal" role="form" method="POST" action="/login">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                         <div class="form-group">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
                 
                     @include('errors.errors')
 
-                    <form class="form-horizontal" role="form" method="POST" action="/auth/register">
+                    <form class="form-horizontal" role="form" method="POST" action="/register">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                         <div class="form-group">
