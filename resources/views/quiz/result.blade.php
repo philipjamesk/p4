ae6eec101bf4b825bfd10dd9aa759591d30faca0
PhilipJames <pjkelnhofer@gmail.com>
2015-12-16 17:18:50 -0500

    Added links to results page

--- resources/views/quiz/result.blade.php
+++ resources/views/quiz/result.blade.php
@@ -3,4 +3,6 @@
 @section('content')
     <h2>Result of Quiz {{ $id }}</h2>
     <h3>You scored {{ number_format($score, 2, '.', '') }}%</h3>
+    <p><a href="/grades">View Grades</a></p>
+    <p><a href="/">Return Home</a></p>
 @stop
