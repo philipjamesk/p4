6210845c3787788bc4afb65da02c2eaa180632cb
PhilipJames <pjkelnhofer@gmail.com>
2015-12-16 17:31:04 -0500

    Added link to confirm delete page

--- resources/views/edit/delete.blade.php
+++ resources/views/edit/delete.blade.php
@@ -4,6 +4,7 @@
     <h2>Are you sure you want to delete {{ $quiz->quiz_name }}?</h2>
     <h3>This action cannot be undone.</h3>
     <a href="/delete/confirm/{{ $quiz->id }}"><button class="btn btn-danger">Delete Quiz</button></a>
+    <p><a href="/edit/{{ $quiz->id }}">Return to edit {{ $quiz->quiz_name }}</a></p>
 @stop
 
 
