1e7a42ac68f039570d103a8f0c074834feb3f5ba
PhilipJames <pjkelnhofer@gmail.com>
2015-12-16 01:54:19 -0500

    Fixed HTML tags; deleted unneeded CSS

--- resources/views/edit/delete.blade.php
+++ resources/views/edit/delete.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h2>Are you sure you want to delete {{ $quiz->quiz_name }}?</h2>
     <h3>This action cannot be undone.</h3>
-    <button class="btn btn-danger"><a href="/delete/confirm/{{ $quiz->id }}">Delete</a></button>
+    <a href="/delete/confirm/{{ $quiz->id }}"><button class="btn btn-danger">Delete</button></a>
 @stop
 
 
