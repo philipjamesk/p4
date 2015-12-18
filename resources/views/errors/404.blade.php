221e5b0dfff442036cbce3964656f0ae063f936f
PhilipJames <pjkelnhofer@gmail.com>
2015-12-17 22:00:39 -0500

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+    Page not found
+@stop
+
+@section('content')
+    <h3>Your page could not be found.</h3>
+    <hr>
+    <p><a href="/">Return home</a>
+@stop
